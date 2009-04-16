a2feebe0f72a8cc0c5c1d29950126f20dcba3ad2
Jacob Seidelin <jseidelin@nihilogic.dk>
2009-04-16 15:56:48 +0200

    initial commit

---
+++ soundmanager/src/make-flash9.bat
@@ -0,0 +1,5 @@
+@REM this builds the soundmanager 2 SWF from source
+@REM using mxmlc from the Adobe open-source Flex SDK
+
+@REM c:\progra~1\flexsdk\bin\mxmlc -use-network=true -o ../swf/soundmanager2_flash9.swf -file-specs SoundManager2_AS3.as
+"C:\Program Files\Adobe\Flex Builder 3\sdks\3.2.0\bin\mxmlc"  -use-network=true -o ../swf/soundmanager2_flash9.swf -file-specs SoundManager2_AS3.as
